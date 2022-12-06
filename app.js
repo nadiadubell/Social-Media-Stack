fdcbc6bb8b80fc8842dd45f5749e8e989bc3a82f
nadiadubell <ndubell01@gmail.com>
2022-12-06 12:49:42 -0600

    getting db ready

--- app.js
+++ app.js
@@ -1,62 +1,69 @@
-require('dotenv').config();
-const cors = require('cors');
-const path = require('path');
-const http = require('http');
-const chalk = require('chalk');
+require("dotenv").config();
+const cors = require("cors");
+const path = require("path");
+const http = require("http");
+const chalk = require("chalk");
 
-const express = require('express');
+const express = require("express");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const morgan = require('morgan');
-app.use(morgan('dev'));
+const morgan = require("morgan");
+app.use(morgan("dev"));
 
 app.use(express.json());
 
 app.use(
   cors({
-    origin: '*'
+    origin: "*",
   })
 );
 
 app.use((req, res, next) => {
-  console.log(chalk.whiteBright('<___Body Logger Start___>'));
+  console.log(chalk.whiteBright("<___Body Logger Start___>"));
   console.log(req.body);
-  console.log(chalk.whiteBright('<___Body Logger End___>'));
+  console.log(chalk.whiteBright("<___Body Logger End___>"));
 
   next();
 });
 
-const apiRouter = require('./api');
-app.use('/api', apiRouter);
+const apiRouter = require("./api");
+const { rebuildDB } = require("./db/seedData");
+app.use("/api", apiRouter);
 
-app.get('*', async (req, res, next) => {
+app.get("*", async (req, res, next) => {
   res.status(404);
   next({
-    error: 'Page not found',
-    message: 'The page you are looking for was not found'
+    error: "Page not found",
+    message: "The page you are looking for was not found",
   });
 });
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
 );
 
 app.use((error, req, res, next) => {
   res.send({
     error: error.error,
-    message: error.message
+    message: error.message,
   });
 });
 
-const PORT = process.env['PORT'] ?? 4000;
+const init = async () => {
+  await rebuildDB();
+};
+
+init();
+
+const PORT = process.env["PORT"] ?? 4000;
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
   console.log(
-    chalk.blueBright('Server is listening on'),
-    chalk.bold.yellowBright('PORT :', PORT),
-    chalk.blueBright('.Concat with Social Stack!')
+    chalk.blueBright("Server is listening on"),
+    chalk.bold.yellowBright("PORT :", PORT),
+    chalk.blueBright(".Concat with Social Stack!")
   );
 });
 
