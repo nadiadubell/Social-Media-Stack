5d10dec9e713f6d0ff86e8fd4ba983dca696c9b1
Nadia DuBell <ndubell01@gmail.com>
2024-07-19 17:09:48 -0500

    chore: modified database connection

--- db/client.js
+++ db/client.js
@@ -1,14 +1,20 @@
 const { Pool } = require("pg");
+require('dotenv').config();
 
 const DEV_MODE = false;
 const DATABASE_URL = process.env.DB_URL;
 const PASSWORD = process.env.DB_PW;
+const PROD_PASSWORD = process.env.PROD_DB_PW;
+
+console.log("Password type:", typeof PASSWORD);
 
 if (DEV_MODE) {
   const connectionString = "https://localhost:5432/dev-social-stack";
 
   const client = new Pool({
-    connectionString,
+    user: 'synergy28',
+    password: PASSWORD,
+    connectionString: connectionString,
     ssl:
       process.env.NODE_ENV === "production"
         ? { rejectUnauthorized: false }
@@ -17,12 +23,12 @@ if (DEV_MODE) {
   module.exports = client;
 } else {
   const client = new Pool({
-    // user: "postgres",
-    // host: "postgres.bfmsluauzbdbzaohecte",
-    // database: DATABASE_URL,
-    // password: PASSWORD, // key from bit.io database page connect menu
-    // port: 5432,
-    connectionString: DATABASE_URL,
+    user: "postgres",
+    host: "aws-0-us-east-1.pooler.supabase.com",
+    database: DATABASE_URL,
+    password: PROD_PASSWORD, // key from bit.io database page connect menu
+    port: 6543,
+    // connectionString: DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
