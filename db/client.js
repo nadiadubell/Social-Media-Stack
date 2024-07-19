399f8fa687f5f78c7c36539ac7c42a9794513499
Nadia DuBell <ndubell01@gmail.com>
2024-07-19 17:37:56 -0500

    chore: added db password into config for client.js

--- db/client.js
+++ db/client.js
@@ -26,7 +26,7 @@ if (DEV_MODE) {
     user: "postgres.bfmsluauzbdbzaohecte",
     host: "aws-0-us-east-1.pooler.supabase.com",
     database: DATABASE_URL,
-    // password: PROD_PASSWORD, // key from bit.io database page connect menu
+    password: PROD_PASSWORD, // key from bit.io database page connect menu
     port: 6543,
     // connectionString: DATABASE_URL,
     ssl: {
